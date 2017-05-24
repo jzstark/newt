00117101eb8abd5bc553e25921ecd451355aa3d2
Rhoajax <rho.ajax@gmail.com>
2017-05-24 15:21:19 +0100

    add basic structure for pacakge

--- examples/keras-mnist.py
+++ examples/keras-mnist.py
@@ -63,11 +63,8 @@ loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=
 newtk.import_weight(model_dir, loaded_model)
 
 # Use the fully loaded model now in Keras
-# ???
-
-
 # Fit the model
 # model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
 # Final evaluation of the model
-#scores = model.evaluate(X_test, y_test, verbose=0)
-# print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+scores = loaded_model.evaluate(X_test, y_test, verbose=0)
+print("Baseline Error: %.2f%%" % (100-scores[1]*100))
---
+++ newt/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from . import newt
+
+__version__ = '0.0.1'
