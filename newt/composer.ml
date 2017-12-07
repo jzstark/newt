9d04bc031e2f354aa63e188dfc1634db18788c57
jrzhao <rho.ajax@gmail.com>
2017-12-07 21:14:50 +0000

    start again

--- newt/composer.ml
+++
@@ -1,9 +0,0 @@
-type t
-type service
-type dag
-
-(* syntax for creating DAG *)
-val make_dag : unit -> dag
-
-(* create new service based on existing services and DAG *)
-val create   : service array -> dag -> service
--- newt/deployer.ml
+++
@@ -1,5 +0,0 @@
-type service
-type config
-
-(* save a service to a new gist (json configuration file)*)
-val deploy : service array -> dag -> string -> unit
