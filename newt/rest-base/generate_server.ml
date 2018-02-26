2c6f7c4c5ece62dd199147ae1253c601af4f53a2
jrzhao <rho.ajax@gmail.com>
2018-02-26 22:12:14 +0000

    abstracting server generation code

--- newt/rest-base/generate_server.ml
+++ newt/rest-base/generate_server.ml
@@ -5,7 +5,40 @@ let save_file file string =
   output_string channel string;
   close_out channel
 
-let filter_str (x, y) = x, Yojson.Basic.Util.to_string y
+let strip_string s =
+  Str.global_replace (Str.regexp "[\r\n\t ]") "" s
+
+let filter_str (x, y) = x, 
+  Yojson.Basic.Util.to_string y
+  |> Str.split (Str.regexp "->")
+  |> List.map strip_string
+
+let decode t = 
+  match t with
+  | "int"    -> fun x -> "int_of_string " ^ x.(0) ^ " in"
+  | "float"  -> fun x -> "float_of_string" ^ x.(0) ^ " in"
+  | "string" -> fun x -> x.(0) ^ " in"
+  | "byte"   -> fun x -> Printf.sprintf "decode_base64 %s %s;" x.(0) x.(1)
+  | _        -> failwith "unsupported type"
+
+let encode t = 
+  match t with
+  | "int"    -> fun x -> "string_of_int " ^ x^ " in"
+  | "float"  -> fun x -> "string_of_float " ^ x ^ " in"
+  | "string" -> fun x -> x ^ " in"
+  | "byte"   -> fun x -> Printf.sprintf "encode_base64 %s in" x
+  | _        -> failwith "unsupported type"
+
+
+let get_funame s = 
+  let lst = String.split_on_char '.' s in
+  List.nth lst 1
+
+let remove_last lst = 
+  List.(lst |> rev |> tl |> rev)
+
+let get_last lst = 
+  List.(lst |> rev |> hd)
 
 let _ = 
 
@@ -15,10 +48,43 @@ let json_lst = Yojson.Basic.from_file "service.json"
 in
 
 let f_str = ref "" in 
-
 List.iteri (fun i (a, _) -> 
   f_str := !f_str ^ "let fn" ^ (string_of_int i) 
     ^ " = " ^ a ^ "\n"
+) json_lst;
+
+let branch_str = ref "" in
+List.iteri (fun i (n, t) -> 
+  let c = ref 0 in
+  let func_str = ref "" in
+  let vars = ref "" in
+  let t = remove_last t in 
+
+  List.iter (fun typ ->
+    let bar = 
+      if (typ <> "byte") then (
+        vars := !vars ^ (Printf.sprintf " v%d" !c);
+        Printf.sprintf "let t%d, v%d = params.(%d) in\n" !c !c !c ^
+        Printf.sprintf "let v%d = %s\n" !c (decode typ [|"v" ^ (string_of_int !c)|]) 
+      ) else (
+        vars := !vars ^ Printf.sprintf " v%d" !c; 
+        c := !c + 1;
+        Printf.sprintf "let t%d, v%d = params.(%d) in\n" (!c - 1) (!c - 1) (!c - 1)^
+        Printf.sprintf "let t%d, v%d = params.(%d) in\n" !c !c !c  ^
+        Printf.sprintf "%s\n" (decode typ 
+            [|"v" ^ (string_of_int (!c - 1) ); "v" ^ (string_of_int !c)|])
+      ) in
+    c := !c + 1;
+    func_str := !func_str ^ bar
+  ) t;
+
+  let header = "| \"/predict/" ^ (get_funame n) ^ "\" -> \n" ^
+    "let params = param_str uri " ^ (string_of_int !c) ^ " in\n" in
+  let foot =  "let result = " ^ n ^ !vars ^ 
+    " in\nlet result = " ^ (encode (get_last t) "result") ^
+    "\nServer.respond_string ~status:`OK ~body:(result ^ \"\") ()\n" 
+  in
+  branch_str := !branch_str ^ header^ !func_str ^ foot;
 ) json_lst;
 
 let output_string = 
@@ -51,14 +117,14 @@ let encode_base64 filename =
   let cmd = \"openssl base64 -in \" ^ filename in
   syscall cmd
 
-let decode_base64 bytestr filename =
+let decode_base64 filename bytestr =
   let tmp_byte = Filename.temp_file \"tempbyte\" \".b64\" in
   save_file tmp_byte bytestr;
   let cmd = \"openssl base64 -d -in \" ^ tmp_byte ^ \" -out \" ^ filename in
   syscall cmd
 
-let param_str uri =
-  let params = Array.make 10 (\"\", \"\") in
+let param_str uri n =
+  let params = Array.make n (\"\", \"\") in
   Array.iteri (fun i t ->
     let p = Uri.get_query_param uri (\"input\" ^ (string_of_int (i + 1))) in
     let p = match p with
@@ -66,26 +132,15 @@ let param_str uri =
       | None   -> failwith \"invalid input\"
     in
     params.(i) <- (t, p)
-  ) [|\"\"; \"\"; \"\"|];
+  ) (Array.make n \"\");
   params
 
 let callback _conn req body =
   let uri = Request.uri req in
   match Uri.path uri with
-  | \"/predict/infer_json\" ->
-    let params = param_str uri in
-    (* Hard-coded *)
-    let _, v1 = params.(0) in (* image name *)
-    let _, v2 = params.(1) in (* image b64 string *)
-    let _, v3 = params.(2) in (* topN *)
-    print_endline v3;
-    print_endline v1;
-    decode_base64 v2 v1; 
-    let result = fn0 v1 |> fn1 ~top:(int_of_string v3) in
-    Server.respond_string ~status:`OK ~body:(result ^ \"\n\") ()
-  | _ ->
-    Server.respond_string ~status:`Not_found ~body:\"Route not found\" ()
-
+"
+^ !branch_str ^
+"
 let server =
   Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ())
 
@@ -94,4 +149,4 @@ let () = ignore (Lwt_main.run server)
 
 in
 
-save_file "server.ml" output_string
+save_file "shit.ml" output_string
